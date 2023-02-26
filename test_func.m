4d0f876306c76d9b2e3a47676e002018a908b9f2
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2023-02-26 20:20:50 +0200

    add huen logic

--- test_func.m
+++ test_func.m
@@ -1,6 +1,6 @@
 function ans = test_func ()
   method = input("enter used wanted method (euler_forward,midpoint,euler_backward,) :\n","s");
-  while strcmp (method ,"euler_forward") != 1 && strcmp (method ,"midpoint")!=1 && strcmp (method ,"euler_backward")!=1
+  while strcmp (method ,"euler_forward") != 1 && strcmp (method ,"midpoint")!=1 && strcmp (method ,"euler_backward")!=1 && strcmp (method ,"huen")!=1
       disp('Enter valid  method pls one of the brackets ones!!!');
       method = input("enter used wanted method (Trap,Simp,Mid,lAnalyzis) :\n","s");
   endwhile
@@ -23,7 +23,7 @@ function ans = test_func ()
     x=x_m(i+1);
     y_exact(i+1)=eval(ex);
     abs_err(i+1)=abs(y_exact(i+1)-y_m(i+1) );
-    rel_err(i+1)=abs(abs_err(i+1)/y_exact(i+1) );
+    rel_err(i+1)=abs(abs_err(i+1)/abs(y_exact(i+1) ) );
   endfor
   y_m
   y_exact
